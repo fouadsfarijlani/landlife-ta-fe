36ff20694f943797a6c5ca413bec6b5ede934ef5
Fouad Sfarijlani <fouad@Admins-MacBook-Pro.local>
2022-11-15 13:22:49 +0100

    added loader to the best method and styled button

--- src/Components/BestMethod.tsx
+++ src/Components/BestMethod.tsx
@@ -1,8 +1,10 @@
-import { Typography } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material';
 import React, { useState, useEffect } from 'react';
+import { useMemo } from 'react';
 import { CONSTANTS } from '../lib';
 import { IMethods } from '../lib';
 import axios from '../requests/axiosMain';
+import Loader from './Loader';
 
 const BestMethod = () => {
     const [methodList, setMethodList] = useState<IMethods[]>();
@@ -27,6 +29,20 @@ const BestMethod = () => {
                     sx={{'margin-top': CONSTANTS.styles.margin_sm}}>
                     Best Methods used for species
             </Typography>
+            {!methodList ? (<Loader />) : (
+                <FormControl fullWidth>
+                    <InputLabel>Methods</InputLabel>
+                    <Select label= "Methods">
+                        {methodList.map((method, i) => (
+                            <MenuItem value={method.method} key={i}>{method.method}</MenuItem>
+                        ))}
+                    </Select>
+                    <Button variant='contained' 
+                        fullWidth sx={{'marginTop': CONSTANTS.styles.margin_sm}}>
+                        Check Method
+                    </Button>
+                </FormControl>
+            )}
         </>
     );
 };
---
+++ src/Components/Loader.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { CircularProgress } from '@mui/material';
+const Loader = () => {
+    return (<CircularProgress color="inherit" />);
+};
+
+export default Loader;
