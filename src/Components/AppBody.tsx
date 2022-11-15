85a8af131903c03d7abb4d68d23a3966f1d3811c
Fouad Sfarijlani <fouad@Admins-MacBook-Pro.local>
2022-11-15 10:59:21 +0100

    Tested some styling on Grid Items inside AppBody

--- src/Components/AppBody.tsx
+++ src/Components/AppBody.tsx
@@ -8,13 +8,18 @@ const AppBody = () => {
             justifyContent={'center'} 
             style={{minHeight: '100vh', marginTop: '24px'}}
             >
-            <Grid item xs={6} md={4}>
+            <Grid item xs={6} md={4} sx={{
+                    'border-right': 'solid black 1px',
+                    'border-top': 'solid black 1px',
+                }}>
                 <Box>
-                    <Button variant="outlined" fullWidth>Get Highest Tree</Button>
-                    <Button variant="outlined" fullWidth>Browse Tree Species</Button>
+                    <Button  fullWidth>Get Highest Tree</Button>
+                    <Button fullWidth>Browse Tree Species</Button>
                 </Box>
             </Grid>
-            <Grid item xs={6} md={8}> HI </Grid>
+            <Grid item xs={6} md={8}
+                sx={{'border-top': 'solid black 1px'}}
+            > HI </Grid>
         </Grid>
     )
 }
